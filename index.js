44094c73d74e01e8b4c3d137dd4528901d801c1e
Casey Spain <spaincasey7@gmail.com>
2019-11-15 21:23:19 -0700

    save

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', process.env.PORT || 5000)
    .use(express.static(__dirname + '/public'))
    .set('views', __dirname + '/views')
    .set('view engine', 'ejs')
-   .get('/', function(req, res){
+   .get('/', function(req, res){h
       res.sendFile('form.html', { root: __dirname + "/public"});
    })
    .get('/calculate', rateCalculator.calculate)
