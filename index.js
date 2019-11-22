98052c9266d3794613b80cecea87d13fda65f38f
Casey Spain <spaincasey7@gmail.com>
2019-11-21 21:07:59 -0700

    tese

--- index.js
+++ index.js
@@ -12,6 +12,41 @@ pool.on('error', (err, client) => {
    process.exit(-1)
  })
 
+// added comment
+app.set('port', process.env.PORT || 5000)
+   .use(express.static(__dirname + '/public'))
+   .set('views', __dirname + '/views')
+   .set('view engine', 'ejs')
+   .get('/', function(req, res){
+      res.sendFile('HomePage.html', { root: __dirname + "/public"});
+   })
+   .get('/getJobs', getJobs)
+   .listen(app.get('port'), function() {
+      console.log('Listening on port: ' + app.get('port'));
+   })
+
+
+function getJobs(req, res) {
+   console.log(req.body.selectpicker);
+   // // First get the person's id
+   // const id = request.query.id;
+
+   // // TODO: We should really check here for a valid id before continuing on...
+
+   // // use a helper function to query the DB, and provide a callback for when it's done
+   // getPersonFromDb(id, function(error, result) {
+   //    // This is the callback function that will be called when the DB is done.
+   //    // The job here is just to send it back.
+
+   //    // Make sure we got a row with the person, then prepare JSON to send back
+   //    if (error || result == null || result.length != 1) {
+   //       response.status(500).json({success: false, data: error});
+   //    } else {
+   //       const person = result[0];
+   //       response.status(200).json(person);
+   //    }
+   // });
+}
 // pool.connect((err, client, done) => {
 //    if (err) throw err
 //    client.query("SELECT * FROM Job JOIN Category USING(id) WHERE cat_name = 'demolition';", [1], (err, res) => {
@@ -31,16 +66,3 @@ pool.on('error', (err, client) => {
 //    }
 //    console.log('user:', res.rows[0])
 // })
-
-// added comment
-app.set('port', process.env.PORT || 5000)
-   .use(express.static(__dirname + '/public'))
-   .set('views', __dirname + '/views')
-   .set('view engine', 'ejs')
-   .get('/', function(req, res){
-      res.sendFile('HomePage.html', { root: __dirname + "/public"});
-   })
-   .get('/calculate', rateCalculator.calculate)
-   .listen(app.get('port'), function() {
-      console.log('Listening on port: ' + app.get('port'));
-   })
