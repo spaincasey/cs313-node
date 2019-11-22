0500a4d03d29840929c21f823b9fe1ca8b1bbe8e
Casey Spain <spaincasey7@gmail.com>
2019-11-21 19:56:07 -0700

    test

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 var app = express();
 
 var rateCalculator = require('./rateCalculator.js');
-const { Pool } = require('pg');
-const connectionString = process.env.DATABASE_URL || 'postgres://pahknpkcrlpedi:d83deec1be30538d2c2adc176dee199285fa790092d60756e52aa0f8fd8938da@ec2-54-243-49-82.compute-1.amazonaws.com:5432/ddi8a0t03prqbk';
+// const { Pool } = require('pg');
+// const connectionString = process.env.DATABASE_URL || 'postgres://pahknpkcrlpedi:d83deec1be30538d2c2adc176dee199285fa790092d60756e52aa0f8fd8938da@ec2-54-243-49-82.compute-1.amazonaws.com:5432/ddi8a0t03prqbk';
 
 
-const pool = new Pool({connectionString: connectionString});
+// const pool = new Pool({connectionString: connectionString});
 
 // added comment
 app.set('port', process.env.PORT || 5000)
