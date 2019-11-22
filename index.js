66bd1c2c1c3f64383884d4281a863be364f7c8d5
Casey Spain <spaincasey7@gmail.com>
2019-11-22 16:00:27 -0700

    test

--- index.js
+++ index.js
@@ -46,7 +46,8 @@ function getJobs(request, response) {
 }
 
 function getJobsFromDb(category, callback) {
-	const sql = "SELECT * FROM Job WHERE category = $1";
+   const sql = "SELECT description, image FROM Job JOIN Category USING(id) WHERE cat_name = $1"
+	// const sql = "SELECT * FROM Job WHERE category = $1";
 	const params = [category];
 	pool.query(sql, params, function(err, result) {
 		if (err) {
