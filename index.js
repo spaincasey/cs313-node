23eb3f62d7e680168b60a7bca18953edcf2fa13b
Casey Spain <spaincasey7@gmail.com>
2019-11-21 21:40:32 -0700

    yrdy

--- index.js
+++ index.js
@@ -13,15 +13,15 @@ pool.on('error', (err, client) => {
  })
 
 // added comment
-app.set('port', process.env.PORT || 5000)
-   .use(express.static(__dirname + '/public'))
-   .set('views', __dirname + '/views')
-   .set('view engine', 'ejs')
-   .get('/', function(req, res){
+app.set('port', process.env.PORT || 5000);
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.get('/', function(req, res){
       res.sendFile('HomePage.html', { root: __dirname + "/public"});
-   })
-   .get('/getJobs', getJobs)
-   .listen(app.get('port'), function() {
+   });
+app.get('/getJobs', getJobs);
+app.listen(app.get('port'), function() {
       console.log('Listening on port: ' + app.get('port'));
    });
 
