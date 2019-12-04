bbd331bd86dc696b30f22820eaae7d4c432231f8
Casey Spain <spaincasey7@gmail.com>
2019-12-03 18:53:11 -0700

    test

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.set('port', process.env.PORT || 5000)
    })
    .get('/getJobs', dbAccess.getJobs)
    .post('/postUser', function(req, res){
-      const first = request.body.first_name;
+      const first = req.body.first_name;
       console.log("First Name: " + first);
    })
    .listen(app.get('port'), function() {
