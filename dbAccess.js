e32b0419f87d54d86c89e93a7a66bf20fcfe68d4
Casey Spain <spaincasey7@gmail.com>
2019-12-13 12:13:48 -0700

    test

--- dbAccess.js
+++ dbAccess.js
@@ -58,13 +58,13 @@ function getUser(req, res) {
  }
  function getUserFromDb(email, callback) {
     const params = [email];
-    var sql = "SELECT * FROM User_app WHERE email = '$1'";
+    var sql = "SELECT * FROM User_app WHERE email = $1";
     pool.query(sql, params, function(err, result) {
         if(err) {
             callback(err, null);
         }
         callback(null, result.rows);
-        console.log(result);
+        console.log(result.rows);
     })  
  }
 
