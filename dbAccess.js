a49dcbc81c3b2b982227fc1ef6efe62e296f1e5e
Casey Spain <spaincasey7@gmail.com>
2019-12-15 23:03:08 -0700

    test

--- dbAccess.js
+++ dbAccess.js
@@ -62,8 +62,8 @@ addReview = async(email, review) => {
     pool.query(sql, params, function(err, results) {
 
     });
-    var sql = "SELECT * FROM Review, User_app WHERE Review.user_app_id = User_app.id;";
-    pool.query(sql, function(err, result) {
+    var sql2 = "SELECT * FROM Review, User_app WHERE Review.user_app_id = User_app.id;";
+    pool.query(sql2, function(err, result) {
     })  
     return new Promise(resolve => {
         setTimeout(() => {
