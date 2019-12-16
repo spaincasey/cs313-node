e8aa6f0f9cdb339249912a08045c2247b8d41e70
Casey Spain <spaincasey7@gmail.com>
2019-12-15 23:18:06 -0700

    test

--- dbAccess.js
+++ dbAccess.js
@@ -49,10 +49,9 @@ postReview = async(req, res) => {
         email = sess.user[0].email;
         console.log("User: " + sess.user);
         console.log("User email: " + email)
-        const result = await addReview(email, review);
-        res.render('pages/reviews', {result: result, user: sess.user});
+        const a = await addReview(email, review);
         console.log(a);
-        // res.send();
+        res.send();
     } catch {
         console.log("Could not add review");
     } 
@@ -63,10 +62,6 @@ addReview = async(email, review) => {
     pool.query(sql, params, function(err, result) {
 
     });
-    var sql2 = "SELECT * FROM Review, User_app WHERE Review.user_app_id = User_app.id;";
-    pool.query(sql2, function(err, result) {
-        return(result.rows);
-    })  
     return new Promise(resolve => {
         setTimeout(() => {
             resolve('Done');
